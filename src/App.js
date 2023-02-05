c45ee14069f58f6c02929c032cf859d84618012e
Abdul(Developer) <0328abdul@gmail.com>
2023-02-05 16:45:31 +0530

    Creating components for news

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import NewsItem from './components/NewsItem';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Welcome to NewsProject
+        <Navbar />
+        <NewsItem />
       </div>
     )
   }
